91d11a9079f79754a1c5671f150885add1ec21b6
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-05-21 17:09:33 -0500

    actualizaciones

--- RepasosDataGridView/Form1.Designer.cs
+++ RepasosDataGridView/Form1.Designer.cs
@@ -31,7 +31,11 @@ namespace RepasosDataGridView
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -53,11 +57,43 @@ namespace RepasosDataGridView
             this.dataGridView1.Size = new System.Drawing.Size(509, 199);
             this.dataGridView1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(774, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Retirar Fila Seleccionada";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(609, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Agregar Fila";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(609, 68);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(509, 199);
+            this.dataGridView2.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 290);
+            this.ClientSize = new System.Drawing.Size(1148, 290);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -65,7 +101,9 @@ namespace RepasosDataGridView
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -74,6 +112,9 @@ namespace RepasosDataGridView
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
--- RepasosDataGridView/Form1.cs
+++ RepasosDataGridView/Form1.cs
@@ -8,13 +8,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Globalization;
+
 namespace RepasosDataGridView
 {
     public partial class Form1 : Form
     {
+
+        private int index;
+        private Random random;
+        private NumberFormatInfo nfi;
+        private DataTable table;
+
         public Form1()
         {
             InitializeComponent();
+
+            index = 1;
+            random = new Random();
+            
+            nfi = new NumberFormatInfo();
+            nfi.NumberGroupSeparator = ",";
+            nfi.NumberDecimalSeparator = ".";
+
+            table = new DataTable();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,6 +49,44 @@ namespace RepasosDataGridView
             table.Rows.Add(5, "Carlos Valencia", "cvalencia@yahoo.es");
 
             dataGridView1.DataSource = table;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PintaGrilla();
+        }
+
+        private void PintaGrilla()
+        {
+            table.Columns.Add("ID", System.Type.GetType("System.Int32"));
+            table.Columns.Add("Producto", System.Type.GetType("System.String"));
+            table.Columns.Add("Stock", System.Type.GetType("System.Int32"));
+            table.Columns.Add("Precio", System.Type.GetType("System.String"));
+
+            for (int i = 0; i < 10; i++)
+            {
+                string producto = string.Format("Producto {0:000}", index);
+                int stock = random.Next(10, 100);
+                double precio = random.Next(100, 200);
+
+                table.Rows.Add(index, producto, stock, precio.ToString("#,###.00", nfi));
+                index++;
+            }
+
+            dataGridView2.DataSource = table;
+
+            // JUSTIFICACIONES
+            dataGridView2.Columns["ID"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView2.Columns["Stock"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView2.Columns["Precio"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            // ANCHO
+            dataGridView2.Columns[0].Width = 50;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
